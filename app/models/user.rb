6770087cf6d9369b789874feefa76d01c8964125
Evgeniy-ES <ees86@yandex.ru>
2021-11-03 16:46:07 +0200

    Attempt 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
 
-  
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
 
   def tests_by_level(level)
     tests.where(level: level)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'users/new'
   root to: "home#index"
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
+  get :logout, to: 'sessions#destroy'
 
   resources :users, only: :create
+  resources :sessions, only: :create
 
 
 #  get '/tests/:category/:title', to: 'tests#search', level: 1

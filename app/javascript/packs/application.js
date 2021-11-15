d5800b2aeaa18b7db534027b63376ec94505591f
Evgeniy-ES <ees86@yandex.ru>
2021-11-15 15:52:59 +0200

    Attempt 1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//
+//=  require rails-ujs
+//= require tu
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap-datarange'
 
 Rails.start()
 Turbolinks.start()

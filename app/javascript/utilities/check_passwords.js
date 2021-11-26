36509c0d4376eaed236713459b190b63c64d2dcc
Evgeniy-ES <ees86@yandex.ru>
2021-11-26 15:35:00 +0200

    Attempt 4

--- app/javascript/utilities/check_passwords.js
+++ app/javascript/utilities/check_passwords.js
@@ -2,7 +2,16 @@ document.addEventListener('turbolinks:load', function() {
 
   var password_confirmation_value = document.querySelector('.password_confirmation')
 
-  if (password_confirmation_value) { password_confirmation_value.addEventListener('keyup', function_check_password) }
+  if (password_confirmation_value) {
+    password_confirmation_value.addEventListener('keyup', function_check_password)
+   } else {
+    var password_for_icon_ivalid = document.querySelector('.text-invalid')
+    var password_conf_for_icon_invalid = document.querySelector('.text-invalid-conf')
+
+    password_for_icon_ivalid.classList.add('hide')
+    password_conf_for_icon_invalid.classList.add('hide')
+  }
+
 
 })
 
@@ -11,14 +20,25 @@ function function_check_password() {
   var passwordConfirmation = document.querySelector('.password_confirmation').value;
   var password = document.querySelector('.password').value;
 
-  var password_for_icon = document.querySelectorAll('input#user_password')
+  var password_for_icon = document.querySelector('.text-valid')
+  var password_conf_for_icon = document.querySelector('.text-valid-conf')
 
-  console.log(passwordConfirmation)
-  console.log(password)
+  var password_for_icon_ivalid = document.querySelector('.text-invalid')
+  var password_conf_for_icon_invalid = document.querySelector('.text-invalid-conf')
 
   if (passwordConfirmation == password) {
-    console.log('!!!')
-    password_for_icon('.octicon-arrow-up').classList.remove('hide')
+    password_for_icon.classList.remove('hide')
+    password_conf_for_icon.classList.remove('hide')
+    password_for_icon_ivalid.classList.add('hide')
+    password_conf_for_icon_invalid.classList.add('hide')
+  } else if (passwordConfirmation.length == 0) {
+    password_for_icon_ivalid.classList.add('hide')
+    password_conf_for_icon_invalid.classList.add('hide')
+  } else {
+    password_for_icon_ivalid.classList.remove('hide')
+    password_conf_for_icon_invalid.classList.remove('hide')
+    password_for_icon.classList.add('hide')
+    password_conf_for_icon.classList.add('hide')
   }
 
 

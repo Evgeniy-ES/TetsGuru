a0de1381a67dce742168a38f4f871b78e40782e1
Evgeniy-ES <ees86@yandex.ru>
2021-12-20 16:41:49 +0300

    Attempt 3

--- app/controllers/my_badges_controller.rb
+++ app/controllers/my_badges_controller.rb
@@ -2,13 +2,7 @@ class MyBadgesController < ApplicationController
   skip_before_action :authenticate_user!, raise: false
 
   def index
-    user_badges = UserBadge.where(user_id: current_user.id)
-    @badge = []
-
-    user_badges.each do |ub|
-      @badge = @badge.push(Badge.find(ub.badge_id))
-    end
-
+    @badge = current_user.badges
     @rule = Rule.all
   end
 end
